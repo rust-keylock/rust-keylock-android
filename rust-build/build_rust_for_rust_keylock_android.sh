3256d5f07ecdc160de453bb84016cb0c7e1efcc5
aston <astonbitecode@gmail.com>
2017-03-28 16:01:01 +0300

    Create subdirectories if needed

--- rust-build/build_rust_for_rust_keylock_android.sh
+++ rust-build/build_rust_for_rust_keylock_android.sh
@@ -8,6 +8,7 @@ echo Entered Base directory $BASEDIR
 ANDROID_RUST="$BASEDIR/rust"
 ANDROID_RUST_KEYLOCK_LIB="$ANDROID_RUST/target/arm-linux-androideabi/release/librustkeylockandroid.so"
 ANDROID_JAVA_NATIVE="$BASEDIR/java/libs/armeabi/"
+mkdir -p $ANDROID_JAVA_NATIVE
 cd $ANDROID_RUST
 xargo build --target=arm-linux-androideabi --release
 
